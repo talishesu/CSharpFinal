a583f69b404ad8cee19555d285578ca2a4f862e9
talishesu <talishesu@code.edu.az>
2021-03-10 21:28:47 +0400

    Methods are added to Interfaces

--- Interfaces/IMarketableProduct.cs
+++ Interfaces/IMarketableProduct.cs
@@ -8,5 +8,19 @@ namespace CSharpFinal.Interfaces
 {
     interface IMarketableProduct
     {
+        //Mehsullar
+        void Products();
+        //Satisdan mehsulun geri qaytarilmasi
+        string ProductSales();
+        //Yeni mehsul elave etmek
+        void AddProduct();
+        //Mehsulun adini,sayini ve meblegini,categoriyasini deyismek (code-a gore tapilacaq)
+        void ChangeProductInfo();
+        //Verilmis kateqoriyaya esasen hemin kateqoriyada olan mehsullarin qaytarilmasi
+        string ProductForCategory();
+        //Verilmis qiymet araligina esasen hemin araliqda olan mehsullarin qaytarilmasi
+        string ProductForPrice();
+        //Verilmis ada esasen mehsullarin search edilib qaytarilmasi
+        void ShowProductForName();
     }
 }
--- Interfaces/IMarketableSales.cs
+++ Interfaces/IMarketableSales.cs
@@ -8,5 +8,19 @@ namespace CSharpFinal.Interfaces
 {
     interface IMarketableSales
     {
+        //Satislar
+        void Sales();
+        //Satis elave etmek
+        void AddSales();
+        //Umumi satisin geri qaytarilmasi
+        string AllSales();
+        //Verilen tarix araligina gore hemin tarix araligina olan satislarin qaytarilmasi
+        string SalesForDate();
+        //Verilen bir tarixe gore hemin tarix (il,ay,gun) ucun olan satislarin qaytarilmasi
+        string SalesForExactDate();
+        //Verilmis mebleg araligina gore edilmis satislarin qaytarilmasi
+        string SalesForPrice();
+        //Verilmis nomreye esasen satisin qaytarilmasi
+        string SalesForNumber();
     }
 }
